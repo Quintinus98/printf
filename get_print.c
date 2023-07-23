d6b0c32113cc5a0c1fd0081ef635e543f4e1fb78
Obodo David <obododavidsop@gmail.com>
2023-07-23 13:23:09 +0100

    Update

--- get_print.c
+++ get_print.c
@@ -13,17 +13,18 @@ int (*get_print(char s))(va_list ap)
 		{'s', print_string},
 		{'%', print_per},
 		{'d', print_int},
-		{'i', print_int}
+		{'i', print_int},
+		{'\0', NULL}
 	};
 	int i;
 
 	i = 0;
-	while (prints[i].per)
+	while (prints[i].per != '\0')
 	{
 		if (prints[i].per == s)
 			return (prints[i].f);
 		i++;
 	}
 
-	return (NULL);
+	return (-1);
 }
