bc6c2a91e0083bf43fb72a243555e84fe5a11724
Obodo David <obododavidsop@gmail.com>
2023-07-23 19:48:59 +0100

    Update not func null

--- printf.c
+++ printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 				return (-1);
 
 			func = get_print(*tmp);
-			if (!func)
+			if (func == NULL)
 				return (-1);
 
 			sum += (func) ? func(ap) : _printf("%%%c", *tmp);
