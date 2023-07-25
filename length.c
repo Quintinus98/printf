8c3cfb1186e875b80c08ddc0107e255eab72efd6
Obodo David <obododavidsop@gmail.com>
2023-07-25 22:33:21 +0100

    Update

--- length.c
+++ length.c
@@ -15,26 +15,6 @@ int xlen(char *s)
 }
 
 /**
- * ilen - get length.
- * @n: number
- * Return: returns the length of a integer.
-*/
-int ilen(int n)
-{
-	unsigned int cnt = 0;
-
-	if (n < 0)
-		n = -n;
-
-	while (n != 0)
-	{
-		n = n / 10;
-		cnt++;
-	}
-	return (cnt);
-}
-
-/**
  * convert - converts an integer to a specified base
  * @val: value to be converted
  * @base: base
--- print_funs2.c
+++ print_funs2.c
@@ -2,7 +2,6 @@
 /**
  * print_int - Prints character
  * @ap: variable list.
- *
  * Return: length of int
 */
 int print_int(va_list ap)
@@ -10,29 +9,46 @@ int print_int(va_list ap)
 	int val = va_arg(ap, int);
 	int len = ilen(val);
 
-	if (val < 0)
-	{
-		_putchar('-');
-		val = -val;
-		len++;
-	}
-	else if (val == 0)
+	if (val <= 0)
 		len++;
 
 	_putint(val);
-
 	return (len);
 }
 
 /**
  * _putint - puts integer
  * @num: number.
- *
  * Return: Nothing
 */
 void _putint(int num)
 {
-	if ((num / 10))
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num / 10)
 		_putint(num / 10);
 	_putchar((num % 10) + '0');
+}
+
+/**
+ * ilen - get length.
+ * @n: number
+ * Return: returns the length of a integer.
+*/
+int ilen(int n)
+{
+	unsigned int cnt = 0;
+
+	if (n < 0)
+		n = -n;
+
+	while (n != 0)
+	{
+		n = n / 10;
+		cnt++;
+	}
+	return (cnt);
 }
